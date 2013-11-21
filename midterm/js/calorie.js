a4e32d6ea1d89cd0c54adca988bc13e5da902825
shreeiyer <ividyar@gmail.com>
2013-11-21 13:50:02 +0000

    Update calorie.js

--- midterm/js/calorie.js
+++ midterm/js/calorie.js
@@ -1,10 +1,9 @@
 
 
-
 setTimeout(fade_out, 1000); // box will fade in 1 secs
 setTimeout(show, 1000); // box 2 will appear after 1 sec delay
 
-$(document).ready(function(){
+
 
 function fade_out() {
  	$("#torso-round").fadeOut().detach();
@@ -17,7 +16,7 @@ function show() {
 	 $('#torso').show();
 
 }
-});
+
 
 function Calculate()
 {
